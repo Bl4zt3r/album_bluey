37ded6ba80bfb9785abbb26b18b2e55bee4cee10
Bl4zt3r <felipe.pinela@usach.cl>
2024-06-15 22:23:31 -0400

    changes

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -3,7 +3,9 @@ import {useState} from 'react';
 import ButtonsView from './ButtonsView';
 const inicio = () => {
     return (
+      
         <div className="App">
+          <h2> hola </h2>
           <header className="App-header">
             <h1>Laminas</h1>
           </header>
